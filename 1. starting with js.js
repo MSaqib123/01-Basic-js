963e7308aec999e2e148d0dee136cf8f448d9665
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-08-05 22:59:57 +0500

    [SAQIB]

---
+++ 1. starting with js.js
@@ -0,0 +1,115 @@
+//-------------- Progaming langauge -------------
+// asaa  programm jis me    data use krna ho baar or
+// but script is not servere  it is client side language
+
+// client side   javascript 
+/* 
+
+
+html , css , javascri  {3 1}
+
+
+comment  multi line
+sdfkasdjfk
+
+*/
+
+
+// comment  single line
+
+/*---------- Javascript build methods ------------- */
+
+// ----- Prompt ------
+//1. alert()
+// alert();  //method    
+
+// Ctrl + /
+
+//2. confirm()
+// confirm("Do you want continue"); 
+
+//3. promp()
+// prompt("Please enter you fullName");
+
+
+//------- console methods -------
+//console   black box
+// console.log("console write");
+// console.error("console write"); //eror
+// console.info("console write");
+// console.warn("console write");
+
+
+
+/*---------- Variables ------------- */
+// var cola ; //  declartion  
+// cola = "pani"; // initilization
+// var name = "SAQIB SAQIBSAQIBSAQIBSAQIBSAQIBSAQIBSAQIBSAQIB awsef asdfasdf asdf afasdf asd fasdfasdfasdfasdfasdfaa dsfasdf";   // 5kb 
+
+// console.log(name)  
+
+
+
+
+//1 line
+//1 line
+//1 line
+//1 line
+//1 line
+//1 line
+//1 line
+//1 line
+//1 line
+//1000 line
+
+// console.log(name)  
+// console.log(name)  // memory   5kb
+// console.log(name)  // memory   5kb
+// console.log(name)  // memory   5kb
+// console.log(name)  // memory   5kb
+// console.log(name)  // memory   5kb
+// console.log(name)  // memory   5kb
+// console.log(name)  // memory   5kb
+// console.log(name)  // memory   5kb
+// console.log(name)  // memory   5kb
+// console.log(name)  // memory   5kb
+
+
+
+
+
+// var age =  15  //  init
+// var isSchoolAllowed = true  // boolean
+
+// varible types
+// integer   234234
+// string    'asdfasdfasdfasdfkasdfkasdf'  "23423423423"
+// bool      true , false  (han , naa)
+// null      khaaliii
+// date      date
+
+
+// array     []
+// object    {}
+
+
+
+// -------------- Task ----------------
+// // 1.   take   firstname , last name from user and  make it fullname
+// var fName = prompt("Please Give firstName"); 
+// var lName = prompt("Please Give LastName");  
+
+// console.log("MY full Name is :: " + fName +" " + lName);   // Concatination    milanaaa
+
+
+
+// // 2.  take 2 number from user and  make   //addition , substraction , multi , divi
+// var fNumber = prompt("Please Give 1st number "); 
+// var lnumber = prompt("Please Give 2nd number ");  
+
+// console.log()
+
+
+// 3.  take 1  confirm value from user and make it  message base on result
+
+
---
+++ 2. variable forms.js
@@ -0,0 +1,39 @@
+  // key  , const ,  dateTime 
+
+
+// --- var ---
+var xyz = 234234;
+var xyz = "asdfasdf"
+var xyz = "asdfasdf"
+var xyz = "asdfasdf"
+var xyz = "asdfasdf"
+var xyz = "asdfasdf"
+var xyz = "asdfasdf"
+var xyz = "asdfasdf"
+var xyz = true
+console.log(xyz)
+
+
+
+// --- let ---
+let abc = 234234;
+//let abc = 234234;
+
+
+// // --- const (fixed) ---- 
+ const CNIC /*dec*/ = 234234234234234; //init
+
+ //reinitilzation
+ //CNIC = 1; //assign
+
+ console.log(CNIC)
+
+
+
+
+//----------------- ES 5 -------------------------
+// var
+
+
+//----------------- ES 6 ------------------------
+// let ,  const 
