007b53fc1ce39229e27a5d7d98e96efafebf3c55
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-08-14 09:57:41 +0500

    [SAQIB] adding new fiels

---
+++ 10. Increment decrement operators.js
@@ -0,0 +1,50 @@
+
+
+let v1 = 21;
+// v1+=1;
+console.log(v1)
+
+
+//===== Increment Operator =======   
+//val++ 
+v1++; //22
+console.log(v1)
+
+
+//===== Post Decrement Operator =======
+//val--
+v1--; //21
+console.log(v1)
+
+
+
+//====================== Post Increment =========================
+let numb = 23;
+// console.log("Incemenrt Operator : ", numb++) //23
+// console.log("Incemenrt Operator : ", numb) //24
+
+
+//====================== Pre Increment =========================
+// ++Value
+console.log("Incemenrt Operator : ", ++numb)
+
+
+
+
+
+
+
+
+//====================== Post decrement =========================
+let numb1 = 23;
+// console.log("decrement Operator : ", numb--) //23  //erorr  
+// console.log(numb1);
+// console.log("Incemenrt Operator : ", numb) //24
+
+
+//====================== Pree d =========================
+// --Value
+console.log("Incemenrt Operator : ", --numb)
+
+
+
---
+++ 11. Truthly and fasly.js
@@ -0,0 +1,34 @@
+//==== falsy ====
+//1. false
+//2. NaN  (not a number)
+//3. undifined
+//4. null
+//5.    '' , ""
+//6.  0
+
+
+//JS  0 value
+// if condition
+if(null /* true */){
+    //code of line
+    console.log("true");
+}
+else{
+    console.log("0 is Falsy Value");
+}
+
+
+
+
+//==== Truthy ====
+//1.  "asdfasdf"  
+//2.  {}    
+//3.  234234
+//4. Array
+//5. Date
+
+
+if(1)
+{
+    //asdfasdfasdf
+}
